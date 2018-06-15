0124b0e099c25c782dd0b892fccf75140cc9c716
Miiaan <miguelantohi@hotmail.com>
2018-06-15 09:10:50 +0200

    2

--- Control2.gd
+++ Control2.gd
@@ -1,7 +1,7 @@
 extends Control
 
 # class member variables go here, for example:
-# var a = 2
+var a = 2
 # var b = "textvar"
 
 func _ready():
@@ -10,6 +10,7 @@ func _ready():
 	pass
 
 #func _process(delta):
+	print(a)
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
